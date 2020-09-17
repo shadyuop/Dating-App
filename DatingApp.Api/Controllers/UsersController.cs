398e5110ec72e06ec7619aea31bb854f391d30f2
es-ShadiSaeed2020@alexu.edu.eg <es-ShadiSaeed2020@alexu.edu.eg>
2020-09-18 00:54:30 +0200

    Api: adding Pagination & Pagination Headers

--- DatingApp.Api/Controllers/UsersController.cs
+++ DatingApp.Api/Controllers/UsersController.cs
@@ -32,12 +32,18 @@ namespace DatingApp.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var users = await _repo.GetUsers(userParams);
             var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+            Response.AddPagination(users.CurrentPage, users.PageSize, 
+                users.TotalCount, users.TotalPages);
+          
             return Ok(usersToReturn);
         }
+
+
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
         {
